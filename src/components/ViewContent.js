069e7c3bfee54eaba3b58f969c007af2ac20fe57
cjmin_n <mymemose@naver.com>
2025-04-07 14:07:44 +0900

    feat: 상세리포트 작업 중

--- src/components/ViewContent.js
+++ src/components/ViewContent.js
@@ -1,10 +1,34 @@
-const ViewContent = ({ role, id, title, score, summary }) => {
+import { useState } from 'react';
+import { useUser } from '../context/UserContext'; 
+
+const ViewContent = ({ role, id, score, summary }) => {
+    const { userInfo, setUserInfo } = useUser();
+    const [name, setName] = useState(userInfo? userInfo.name : '');
+
     return (
         <main className="l-view">
             <div className="inner">
                 {/* ✅ 공통 영역 */}
-                <h2>{title}</h2>
-                <p>매칭 점수: {score}</p>
+                <h2 className='sub-tit'>{name}님의 Ai매칭 결과</h2>
+                <h3>Ai MATCHING REPORT</h3>
+                <div className="round-box">
+                    {role === 'HR' ? (
+                        <p>
+                            채용공고와 지원자가 얼마나 일치하는지 <strong>Ai 매칭률과
+                            함께 추천사유</strong>를 속시원히 알려드립니다.
+                        </p>
+                    ) : (
+                        <p>
+                            나와 공고가 얼마나 일치하는지 <strong>Ai 매칭률과
+                            함께 추천사유</strong>를 속시원히 알려드립니다.
+                        </p>
+                    )}
+                </div>
+                <small>*등록하신 이력서를 분석한 결과로, 실제 결과와 다를 수 있습니다.</small>
+                <h4>총평</h4>
+
+
+                <p>{score}</p>
                 <p>요약: {summary}</p>
                 <p>게시물 ID: {id}</p>
 
