6a18c92e5af6833f68a840102485b3147c35ba7b
cjmin_n <mymemose@naver.com>
2025-04-07 09:14:10 +0900

    fix: userInfo 수정 보류

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,11 +1,29 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import { jwtDecode } from 'jwt-decode';
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [userInfo, setUserInfo] = useState(null);
-  
+
+  // useEffect(() => {
+  //   const token = localStorage.getItem('accessToken');
+  //   if (token) {
+  //     try {
+  //       const decoded = jwtDecode(token);
+  //       setUserInfo({
+  //         name: decoded?.name,
+  //         email: decoded?.sub,
+  //         phone: decoded?.phone,
+  //         credit: decoded?.credit,
+  //         role: decoded?.role,
+  //       });
+  //       console.log(decoded?.name, decoded?.sub, decoded?.phone,decoded?.credit, decoded?.role)
+  //     } catch (e) {
+  //       console.error('❌ 토큰 파싱 실패:', e);
+  //     }
+  //   }   
+  // }, []);
 
   const updateCredit = (newCredit) => {
     setUserInfo((prev) => ({
@@ -20,11 +38,11 @@ export const UserProvider = ({ children }) => {
       const decoded = jwtDecode(token);
       setUserInfo((prev) => ({
         ...prev,
-        name: decoded?.name || prev?.name,
-        email: decoded?.sub || prev?.email,
-        phone: decoded?.phone || prev?.phone,
-        credit: decoded?.credit || prev?.credit,
-        // role: decoded?.role || prev?.role
+        name: decoded?.name ?? prev?.name,
+        email: decoded?.sub ?? prev?.email,
+        phone: decoded?.phone ?? prev?.phone,
+        credit: decoded?.credit ?? prev?.credit,
+        // role: decoded?.role ?? prev?.role
       }));
     } catch (err) {
       console.error('❌ 토큰 디코딩 실패:', err);
