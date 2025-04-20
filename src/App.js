bf1b5eb1e7a32913d026817c08cb3df43a0b10f1
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2025-04-20 22:39:04 +0900

    Revert "chore:튜토리얼 컨텍스트 생성"
    This reverts commit b63f0f65c0db40f4fae37d27bc7ac820c264411d.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import { MatchProvider } from './context/MatchContext';
-import { TutorialProvider } from './context/TutorialContext';
 import { useEffect } from "react";
 import Layout from "./layout/Layout";
 import Main from "./pages/Main";
@@ -77,11 +76,9 @@ function App() {
     return (
       <>
         <MatchProvider>
-          <TutorialProvider>
-            <BrowserRouter>
-              <AppRoutes />
-            </BrowserRouter>
-          </TutorialProvider>
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
         </MatchProvider>
         <ToastContainer position="top-center" autoClose={1000} style={{ zIndex: 11002 }}/>
       </>
